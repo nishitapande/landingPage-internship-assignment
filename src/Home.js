ed9356d7c08bf39c3f196673f9d0cc9b9aca7b4c
Nishita Pande <89330962+nishitapande@users.noreply.github.com>
2024-06-05 22:23:42 +0530

    added styles

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ import KeyFeatures from "./components/KeyFeatures";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Review from "./components/Review";
-import Metrics from "./components/Metrics";
+
 import Companies from "./components/Companies";
 
 const Home = () => {
